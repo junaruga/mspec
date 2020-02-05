3e8f6341d4664bf0b736c7039722add49f6f9dd1
Jun Aruga <jaruga@redhat.com>
2020-02-05 01:14:14 +0100

    Add .travis.yml

--- spec/matchers/block_caller_spec.rb
+++ spec/matchers/block_caller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'mspec/expectations/expectations'
 require 'mspec/matchers'
+require 'socket'
 
 describe BlockingMatcher do
   it 'matches when a Proc blocks the caller' do
@@ -9,5 +10,9 @@ describe BlockingMatcher do
 
   it 'does not match when a Proc does not block the caller' do
     BlockingMatcher.new.matches?(proc { 1 }).should == false
+  end
+
+  it 'does not match when the Proc Socket.ip_address_list does not block the caller' do
+    BlockingMatcher.new.matches?(proc { Socket.ip_address_list }).should == false
   end
 end
